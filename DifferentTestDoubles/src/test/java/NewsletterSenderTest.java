a403bc15873b71d938d05f755eb429d2d6b6dd05
utsav-dev <60022819+utsav-dev@users.noreply.github.com>
2024-02-03 09:40:32 +0530

    MOCK/STUB/SPY
    Different Test Doubles

--- DifferentTestDoubles/src/test/java/NewsletterSenderTest.java
+++ DifferentTestDoubles/src/test/java/NewsletterSenderTest.java
@@ -1,3 +1,9 @@
+/*#Utsav
+TEST DOUBLES <>
+DEFINITION : They shield the test class from the external dependencies.
+
+*/
+
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,16 +14,24 @@ import static org.mockito.Mockito.*;
 
 public class NewsletterSenderTest {
 
+
+    /*     MOCK    */
     @Test
     public void constructorAssignsDatabase(){
 
-        MessagingEngine messagingEngine = mock(MessagingEngine.class);
+
+       /* #Utsav mock(.class) => returns a nullified version of the object for that class.*/
+        MessagingEngine messagingEngine = mock(MessagingEngine.class); /*#Utsav We use mock here because the MessagingEngine is just passed a param and not showing any behavior */
         SubscribersDatabase subscribersDatabase = new SubscribersDatabase();
         NewsletterSender newsletterSender = new NewsletterSender(subscribersDatabase, messagingEngine);
 
         assertEquals(subscribersDatabase, newsletterSender.getSubscribersDatabase());
     }
 
+
+    /*    STUB    */
+    /* STUB is MOCK + [instructions on how to respond certain method calls]
+    /* STUB v/s MOCK => MOCK returns simplyh the nullified values whereas the STUB returns the certain values defined for method calls */
     @Test
     public void numberOfSubscribers(){
 
@@ -32,6 +46,12 @@ public class NewsletterSenderTest {
         assertEquals(3, sender.numberOfSubscribers());
     }
 
+    /*    SPY    */
+
+    /* represents the real object 
+    SPY v/s REGULAR INSTANCE
+    spy can define values for certain method calls */
+
     @Test(expected = ZeroSubscribersException.class)
     public void zeroSubscribersThrown(){
 
@@ -44,3 +64,13 @@ public class NewsletterSenderTest {
 
     }
 }
+
+
+
+
+
+NOTES:
+MOCK = nullfied instance which does not have any role to play in the test
+STUB = MOCK + defined values on certain method calls
+SPY = STUB + regualr instace behaviour
+
