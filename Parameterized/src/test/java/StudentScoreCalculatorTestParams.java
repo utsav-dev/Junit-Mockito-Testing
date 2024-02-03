9deb3238e57a8461724da34ecf6bfa426bdf4d1e
utsav-dev <60022819+utsav-dev@users.noreply.github.com>
2024-02-03 08:45:06 +0530

    Adding the notes for Parameterized Testing

--- Parameterized/src/test/java/StudentScoreCalculatorTestParams.java
+++ Parameterized/src/test/java/StudentScoreCalculatorTestParams.java
@@ -5,9 +5,12 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(JUnitParamsRunner.class)
+@RunWith(JUnitParamsRunner.class) /*#Utsav Step-1: annotation used for paramtzd tests*/
 public class StudentScoreCalculatorTestParams {
 
+    /*#Utsav Step-3: Create a methods which returns an array of Objects which can be passed to the test method.
+    This can be passed as a param to @Parameters annotation.
+    NOTE- this is a generic pattern shown so you can re-use it.*/
     private static Object[] testValues(){
 
         return new Object[]{
@@ -25,7 +28,8 @@ public class StudentScoreCalculatorTestParams {
     }
 
     @Test
-    @Parameters(method = "testValues")
+    @Parameters(method = "testValues")/*#Utsav Step-4: This annotation is a must with a testValues() name*/
+    /* #Utsav Step-2: Create a test method with required params of the original method and am extra expected param*/
     public void studentScoreCalculator(int mathsScore, int literacyScore, int expectedScore){
 
         StudentScoreCalculator sc = new StudentScoreCalculator();
